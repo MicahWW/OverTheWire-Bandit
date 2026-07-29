4cd3c9521fea032e1d48c81024b38a4b5830f2f6
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2026-07-28 20:45:03 -0500

    fully automate

--- scripts/13.sh
+++ scripts/13.sh
@@ -1,2 +1,13 @@
-ssh -p 2220 -i ~/sshkey.private  bandit14@bandit.labs.overthewire.org \
+dir=`mktemp -d`
+cd $dir
+echo $dir
+
+scp -P 2220 bandit13@bandit.labs.overthewire.org:~/sshkey.private ./sshkey.private
+# Ensures the private key has the correct permissions as ssh will refuse to use
+# a private key with open permissions.
+chmod 600 ./sshkey.private
+
+ssh -p 2220 -i ./sshkey.private  bandit14@bandit.labs.overthewire.org \
     "cat /etc/bandit_pass/bandit14"
+
+rm -rf $dir
